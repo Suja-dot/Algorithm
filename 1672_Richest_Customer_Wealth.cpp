caffb0390323d34af1feaa35fa658a95b46f835b
Suja-dot <79191046+Suja-dot@users.noreply.github.com>
2021-03-11 13:46:37 +0900

    Create 1672_Richest_Customer_Wealth.cpp

---
+++ 1672_Richest_Customer_Wealth.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int ans = 0;
+        int sum;
+        for(int i=0;i<accounts.size();i++){
+            sum = 0;
+            for(int j=0;j<accounts[i].size();j++) sum += accounts[i][j]; 
+            if(sum>ans) ans = sum;
+        }
+        return ans;
+    }
+};
